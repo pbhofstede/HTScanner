4c4ec588c07dd1849a92d06782ab9bc578642ba0
Harry de Jonge <h.a.dejonge@syntri.nl>
2012-05-09 22:57:31 +0200

    Tja...

--- HTScanner.dpr
+++ HTScanner.dpr
@@ -2,6 +2,11 @@ program HTScanner;
 
 uses
   FastMM4,
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
   Forms,
   FormMain in 'FormMain.pas' {frmHTScanner},
   formAutoScout in 'formAutoScout.pas' {frmAutoScout},
@@ -9,12 +14,14 @@ uses
   formKiesJeudcompetities in 'formKiesJeudcompetities.pas' {frmKiesReeks},
   FormPlayerInfo in 'FormPlayerInfo.pas' {frmPlayerInfo},
   FornNewName in 'FornNewName.pas' {frmNewName},
-  FormNTScouting in 'FormNTScouting.pas' {frmNTScouting};
+  FormNTScouting in 'FormNTScouting.pas' {frmNTScouting},
+  FormKiesLichting in 'FormKiesLichting.pas' {frmKiesLichting};
 
 { *.RES}
 
 begin
   Application.Initialize;
+  Application.Title := 'Hattrick Scanner';
   Application.CreateForm(TfrmHTScanner, frmHTScanner);
   Application.Run;
 end.
